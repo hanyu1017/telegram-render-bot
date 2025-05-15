95cc01ac19d706e06384dc9332513007702aa06d
hanyu1017 <edison0929839399@gmail.com>
2025-05-15 15:04:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+import axios from 'axios';
+
+methods: {
+  async fetchUserData() {
+    try {
+      const response = await axios.get('https://your-api-endpoint.com/user-data');
+      this.userData = response.data;
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
